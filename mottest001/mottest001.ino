0a91ef26cdd9c5f8d26a3fa72130400803fcd171
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2020-12-25 15:50:53 +0800

    Update mottest001.ino

--- mottest001/mottest001.ino
+++ mottest001/mottest001.ino
@@ -1,24 +1,28 @@
 #include <Servo.h>
 Servo motor0;
 
-
 void setup() {
   Serial.begin(9600);
   Serial.println("OG!");
-  motor0.attach(9);
-  for (int i = 0; i<1502;i++){
+  motor0.attach(11);
+  /*for (int i = 0; i<1502;i++){
     motor0.writeMicroseconds(i);
   }
   for (int i = 0; i<499;i++){
     motor0.writeMicroseconds(1500-i);
-  }
+  }*/
   motor0.writeMicroseconds(1000);
   Serial.println("FSED!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-for (int i = 1000; i<2000;i++){
+  for (int i = 1200; i<2000;i++){
     motor0.writeMicroseconds(i);
+    delay(16);
+  }
+  for (int i = 2000; i>1200;i--){
+    motor0.writeMicroseconds(i);
+    delay(16);
   }
 }
